b018dba68d201eff368fbdb568f6790147a1d67b
Pavel Egorov <xoposhiy@mail.ru>
2014-02-14 17:41:41 +0600

    Fix strange hunspell issues. Add sqr to dictionary.

--- stylist/Speller.cs
+++ stylist/Speller.cs
@@ -6,6 +6,9 @@ namespace stylist
 {
 	public class Speller : IDisposable
 	{
+		private static string dictionaryPath;
+		private static Speller instance;
+
 		public Speller(string path)
 		{
 			Hunspell = new Hunspell(Path.Combine(path, "en_GB.aff"), Path.Combine(path, "en_GB.dic"));
@@ -15,22 +18,23 @@ namespace stylist
 		public Hunspell Hunspell { get; private set; }
 		public string[] Antiwords { get; private set; }
 
-		public static Speller Instance;
+		public static Speller Instance { get { return instance ?? (instance = new Speller(dictionaryPath)); } }
+
+		public void Dispose()
+		{
+			Hunspell.Dispose();
+		}
 
 		public static Speller Initialize(string path)
 		{
-			return Instance ?? (Instance = new Speller(path));
+			dictionaryPath = path;
+			return Instance;
 		}
 
 		public static void DisposeInstance()
 		{
 			if (Instance != null)
 				Instance.Dispose();
-		}
-
-		public void Dispose()
-		{
-			Hunspell.Dispose();
 		}
 	}
 }
